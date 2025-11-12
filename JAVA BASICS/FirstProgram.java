94c9efc4662fd82089e387658ac0b780cea5f620
YUVARAJ K <yuvarajyj67@gmail.com>
2025-11-12 20:30:20 +0530

    Delete JAVA BASICS directory

--- JAVA BASICS/FirstProgram.java
+++
@@ -1,9 +0,0 @@
-//Demonstration of simple java program
-package day1.basics;
-import java.util.LinkedList ; //all subpackages
-
-public class FirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
