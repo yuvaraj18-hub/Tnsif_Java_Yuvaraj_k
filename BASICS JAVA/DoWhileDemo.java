143c46e5c4f772f124b7ead4f88a21b47151e07d
YUVARAJ K <yuvarajyj67@gmail.com>
2025-11-12 20:32:01 +0530

    JAVA

---
+++ BASICS JAVA/DoWhileDemo.java
@@ -0,0 +1,12 @@
+//Program to demonstrate  do while loop
+package day1.basics;
+
+public class DoWhileDemo {
+	public static void main(String[] args) {
+		int i = 11;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 1);
+	}
+}
