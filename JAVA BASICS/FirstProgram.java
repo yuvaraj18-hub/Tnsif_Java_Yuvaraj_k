b2ed7fadd0b82fbdb7fcb9196094c8ff4cfd2df7
YUVARAJ K <yuvarajyj67@gmail.com>
2025-11-12 20:28:06 +0530

    Add files via upload

---
+++ JAVA BASICS/FirstProgram.java
@@ -0,0 +1,9 @@
+//Demonstration of simple java program
+package day1.basics;
+import java.util.LinkedList ; //all subpackages
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ JAVA BASICS/IfDemo.java
@@ -0,0 +1,17 @@
+//Program to guess a number in a given range
+package day1.basics;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+
+		int x = 6;
+		if (x != 5) {{
+			System.out.println("Value of x is not 5");
+			}}
+
+		else {
+		System.out.println("Value of x is 5");
+	}
+}}
+
